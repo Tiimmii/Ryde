2f8b0fa58b31868e9a358bac3ee5f22536790868
Timmi <daniel@anyapp.ltd>
2025-05-20 20:11:11 +0100

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex justify-center items-center h-full">
+      <Text className="text-red-500">Uber Clone</Text>
+    </SafeAreaView>
+  );
+}
