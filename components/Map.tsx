a45b4b37d3f393c24863f94fa94467da2bdaf5f2
Timmi <200928487+tiiimmiii@users.noreply.github.com>
2025-06-06 21:07:38 +0100

    Finalyzing set up

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,61 +1,54 @@
 import React, { useEffect, useState } from "react";
-// import { ActivityIndicator, Text, View } from "react-native";
-import MapView, {Marker, PROVIDER_DEFAULT } from "react-native-maps";
-// import MapViewDirections from "react-native-maps-directions";
+import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
 import { icons } from "@/constants";
 import { useFetch } from "@/lib/fetch";
 import { useDriverStore, useLocationStore } from "@/store";
-import { calculateRegion, generateMarkersFromData } from "@/lib/map";
-import { MarkerData } from "@/types/types";
-// import {
-//   calculateDriverTimes,
-//   calculateRegion,
-//   generateMarkersFromData,
-// } from "@/lib/map";
-// import { useDriverStore, useLocationStore } from "@/store";
-// import { Driver, MarkerData } from "@/types/types";
+import { calculateRegion, generateMarkersFromData, calculateDriverTimes } from "@/lib/map";
+import { Driver, MarkerData } from "@/types/types";
+import { ActivityIndicator, Text, View } from "react-native";
 
-// const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
+const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
 
-const drivers = [
-    {
-        "driver_id": 1,
-        "first_name": "James",
-        "last_name": "Wilson",
-        "profile_image_url": "https://ucarecdn.com/dae59f69-2c1f-48c3-a883-017bcf0f9950/-/preview/1000x666/",
-        "car_image_url": "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
-        "car_seats": 4,
-        "rating": "4.80"
-    },
-    {
-        "driver_id": 2,
-        "first_name": "David",
-        "last_name": "Brown",
-        "profile_image_url": "https://ucarecdn.com/6ea6d83d-ef1a-483f-9106-837a3a5b3f67/-/preview/1000x666/",
-        "car_image_url": "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
-        "car_seats": 5,
-        "rating": "4.60"
-    },
-    {
-        "driver_id": 3,
-        "first_name": "Michael",
-        "last_name": "Johnson",
-        "profile_image_url": "https://ucarecdn.com/0330d85c-232e-4c30-bd04-e5e4d0e3d688/-/preview/826x822/",
-        "car_image_url": "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
-        "car_seats": 4,
-        "rating": "4.70"
-    },
-    {
-        "driver_id": 4,
-        "first_name": "Robert",
-        "last_name": "Green",
-        "profile_image_url": "https://ucarecdn.com/fdfc54df-9d24-40f7-b7d3-6f391561c0db/-/preview/626x417/",
-        "car_image_url": "https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/",
-        "car_seats": 4,
-        "rating": "4.90"
-    }
-]
+// const drivers = [
+//     {
+//         "driver_id": 1,
+//         "first_name": "James",
+//         "last_name": "Wilson",
+//         "profile_image_url": "https://ucarecdn.com/dae59f69-2c1f-48c3-a883-017bcf0f9950/-/preview/1000x666/",
+//         "car_image_url": "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
+//         "car_seats": 4,
+//         "rating": "4.80"
+//     },
+//     {
+//         "driver_id": 2,
+//         "first_name": "David",
+//         "last_name": "Brown",
+//         "profile_image_url": "https://ucarecdn.com/6ea6d83d-ef1a-483f-9106-837a3a5b3f67/-/preview/1000x666/",
+//         "car_image_url": "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
+//         "car_seats": 5,
+//         "rating": "4.60"
+//     },
+//     {
+//         "driver_id": 3,
+//         "first_name": "Michael",
+//         "last_name": "Johnson",
+//         "profile_image_url": "https://ucarecdn.com/0330d85c-232e-4c30-bd04-e5e4d0e3d688/-/preview/826x822/",
+//         "car_image_url": "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
+//         "car_seats": 4,
+//         "rating": "4.70"
+//     },
+//     {
+//         "driver_id": 4,
+//         "first_name": "Robert",
+//         "last_name": "Green",
+//         "profile_image_url": "https://ucarecdn.com/fdfc54df-9d24-40f7-b7d3-6f391561c0db/-/preview/626x417/",
+//         "car_image_url": "https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/",
+//         "car_seats": 4,
+//         "rating": "4.90"
+//     }
+// ]
 
 const Map = () => {
   const {
@@ -66,7 +59,7 @@ const Map = () => {
   } = useLocationStore();
   const { selectedDriver, setDrivers } = useDriverStore();
 
-//   const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
+  const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
   const [markers, setMarkers] = useState<MarkerData[]>([]);
 
   useEffect(() => {
@@ -83,23 +76,23 @@ const Map = () => {
     }
   }, [drivers, userLatitude, userLongitude]);
 
-//   useEffect(() => {
-//     if (
-//       markers.length > 0 &&
-//       destinationLatitude !== undefined &&
-//       destinationLongitude !== undefined
-//     ) {
-//       calculateDriverTimes({
-//         markers,
-//         userLatitude,
-//         userLongitude,
-//         destinationLatitude,
-//         destinationLongitude,
-//       }).then((drivers) => {
-//         setDrivers(drivers as MarkerData[]);
-//       });
-//     }
-//   }, [markers, destinationLatitude, destinationLongitude]);
+  useEffect(() => {
+    if (
+      markers.length > 0 &&
+      destinationLatitude !== undefined &&
+      destinationLongitude !== undefined
+    ) {
+      calculateDriverTimes({
+        markers,
+        userLatitude,
+        userLongitude,
+        destinationLatitude,
+        destinationLongitude,
+      }).then((drivers) => {
+        setDrivers(drivers as MarkerData[]);
+      });
+    }
+  }, [markers, destinationLatitude, destinationLongitude]);
 
   const region = calculateRegion({
     userLatitude,
@@ -108,19 +101,19 @@ const Map = () => {
     destinationLongitude,
   });
 
-//   if (loading || (!userLatitude && !userLongitude))
-//     return (
-//       <View className="flex items-center justify-between w-full">
-//         <ActivityIndicator size="small" color="#000" />
-//       </View>
-//     );
+  if (loading || (!userLatitude && !userLongitude))
+    return (
+      <View className="flex items-center justify-between w-full">
+        <ActivityIndicator size="small" color="#000" />
+      </View>
+    );
 
-//   if (error)
-//     return (
-//       <View className="flex items-center justify-between w-full">
-//         <Text>Error: {error}</Text>
-//       </View>
-//     );
+  if (error)
+    return (
+      <View className="flex items-center justify-between w-full">
+        <Text>Error: {error}</Text>
+      </View>
+    );
 
   return (
     <MapView
@@ -138,7 +131,7 @@ const Map = () => {
         right: 0,
         bottom: 0,
       }}
-      
+
     >
       {markers.map((marker) => (
         <Marker
@@ -154,7 +147,7 @@ const Map = () => {
         />
       ))}
 
-      {/* {destinationLatitude && destinationLongitude && (
+      {destinationLatitude && destinationLongitude && (
         <>
           <Marker
             key="destination"
@@ -179,7 +172,7 @@ const Map = () => {
             strokeWidth={2}
           />
         </>
-      )} */}
+      )}
     </MapView>
   );
 };
